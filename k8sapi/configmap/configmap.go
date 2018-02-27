a6b4d222e46a42ef5e7528170ec7d69152f93f5e
Wu Chao <89212550@qq.com>
2018-02-27 10:17:44 +0800

    add k8sapi configmap

--- k8sapi/configmap/configmap.go
+++ k8sapi/configmap/configmap.go
@@ -1,21 +1,12 @@
 package configmap
 
 import (
-	"github.com/pivotal-golang/lager"
-	"os"
-	"github.com/gin-gonic/gin"
-	"github.com/asiainfoLDP/datafoundry-gw/pkg"
-)
-
-/*
-import (
 	"os"
 	"io/ioutil"
 	"github.com/gin-gonic/gin"
 	"github.com/pivotal-golang/lager"
 	"github.com/asiainfoLDP/datafoundry-gw/pkg"
 	oapi "github.com/asiainfoLDP/datafoundry-gw/apirequest"
-	"fmt"
 )
 
 var logger lager.Logger
@@ -25,34 +16,146 @@ func init() {
 	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
 }
 
-
-func GetAllProjects(c *gin.Context){
-token := pkg.GetToken(c)
-req,err := oapi.GenRequest("GET","/oapi/v1/projects",token,nil)
-if err != nil{
-logger.Error("Get All Projects Fail",err)
-}
-result, _:= ioutil.ReadAll(req.Body)
-defer req.Body.Close()
-c.Data(req.StatusCode, "application/json",result)
-}
-
-func WatchAProject(c *gin.Context) {
-
-token := pkg.GetToken(c)
-name := c.Param("name")
-oapi.WSRequest("/oapi/v1/watch/projects/"+name,token,c.Writer,c.Request)
+func CreateConfigMap(c *gin.Context){
+	token := pkg.GetToken(c)
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	//调用原生接口
+	req,err := oapi.GenRequest("POST","/api/v1/configmaps",token,rBody)
+	if err != nil{
+		logger.Error("Create A ConfigMap Fail",err)
+	}
+	//返回结果JSON格式
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
 }
 
-Create a ConfigMap
-Create a ConfigMap in a namespace
-Get a ConfigMap in a namespace
-Get all ConfigMaps
-Get all ConfigMaps in a namespace
-Watch a ConfigMap in a namespace
-Watch all ConfigMaps
-Watch all ConfigMaps in a namespace
-Update a ConfigMap in a namespace
-Patch a ConfigMap in a namespace
-Delete a ConfigMap in a namespace
-Delete all ConfigMaps in a namespace
+func CreateConfigMapNS(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	//调用原生接口
+	req,err := oapi.GenRequest("POST","/api/v1/namespaces/"+namespace+"/configmaps",token,rBody)
+	if err != nil{
+		logger.Error("Create A ConfigMap In A Namespace Fail",err)
+	}
+	//返回结果JSON格式
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func GetConfigMapNS(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	req,err := oapi.GenRequest("GET","/api/v1/namespaces/"+namespace+"/configmaps/"+name,token,nil)
+	if err != nil{
+		logger.Error("Get A ConfigMap In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func GetAllConfigMap(c *gin.Context){
+	token := pkg.GetToken(c)
+	req,err := oapi.GenRequest("GET","/api/v1/configmaps",token,nil)
+	if err != nil{
+		logger.Error("Get All ConfigMap Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func GetAllConfigMapNS(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	req,err := oapi.GenRequest("GET","/api/v1/namespaces/"+namespace+"/configmaps",token,nil)
+	if err != nil{
+		logger.Error("Get All ConfigMap In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func WatchConfigMapNS(c *gin.Context){
+
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	oapi.WSRequest("/api/v1/watch/namespaces/"+namespace+"/configmaps/"+name,token,c.Writer,c.Request)
+
+}
+
+func WatchAllConfigMap(c *gin.Context){
+
+	token := pkg.GetToken(c)
+	oapi.WSRequest("/api/v1/watch/configmaps",token,c.Writer,c.Request)
+
+}
+
+func WatchAllConfigMapNS(c *gin.Context){
+
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	oapi.WSRequest("/api/v1/watch/namespaces/"+namespace+"/configmaps",token,c.Writer,c.Request)
+
+}
+
+func UpdateConfigMapNS(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	req,err := oapi.GenRequest("PUT","/api/v1/namespaces/"+namespace+"/configmaps/"+name,token,rBody)
+	if err != nil{
+		logger.Error("Update A ConfigMap In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func PatchConfigMapNS(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	req,err := oapi.GenRequest("PATCH","/api/v1/namespaces/"+namespace+"/configmaps/"+name,token,rBody)
+	if err != nil{
+		logger.Error("Patch A ConfigMap In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func DeleteConfigMapNS(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	req,err := oapi.GenRequest("DELETE","/api/v1/namespaces/"+namespace+"/configmaps/"+name,token,rBody)
+	if err != nil{
+		logger.Error("Delete A ConfigMap In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func DeleteAllConfigMapNS(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	req,err := oapi.GenRequest("DELETE","/api/v1/namespaces/"+namespace+"/configmaps",token,rBody)
+	if err != nil{
+		logger.Error("Delete All ConfigMap In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}

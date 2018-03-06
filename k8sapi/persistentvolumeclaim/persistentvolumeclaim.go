e1eecec6f5e86a53293a86d112e1b7264b879caa
Wu Chao <89212550@qq.com>
2018-03-06 18:08:54 +0800

    add persistentvolumeclaim

---
+++ k8sapi/persistentvolumeclaim/persistentvolumeclaim.go
@@ -0,0 +1,239 @@
+package persistentvolumeclaim
+
+import (
+	"os"
+	"io/ioutil"
+	"github.com/gin-gonic/gin"
+	"github.com/pivotal-golang/lager"
+	"github.com/asiainfoLDP/datafoundry-gw/pkg"
+	oapi "github.com/asiainfoLDP/datafoundry-gw/apirequest"
+)
+
+var logger lager.Logger
+
+func init() {
+	logger = lager.NewLogger("api_v1_PersistentVolumeClaim")
+	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
+}
+
+func CreatePVC(c *gin.Context){
+	token := pkg.GetToken(c)
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	//调用原生接口
+	req,err := oapi.GenRequest("POST","/api/v1/persistentvolumeclaims",token,rBody)
+	if err != nil{
+		logger.Error("Create A PersistentVolumeClaim Fail",err)
+	}
+	//返回结果JSON格式
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func CreatePVCns(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	//调用原生接口
+	req,err := oapi.GenRequest("POST","/api/v1/namespaces/"+namespace+"/persistentvolumeclaims",token,rBody)
+	if err != nil{
+		logger.Error("Create A PersistentVolumeClaim In A Namespace Fail",err)
+	}
+	//返回结果JSON格式
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func GorWPVCns(c *gin.Context){
+	if pkg.IsWebsocket(c){
+		watchPVCns(c)
+	}else{
+		getPVCns(c)
+	}
+}
+
+func GorWAllPVC(c *gin.Context){
+	if pkg.IsWebsocket(c){
+		watchAllPVC(c)
+	}else{
+		getAllPVC(c)
+	}
+}
+
+func GorWAllPVCns(c *gin.Context){
+	if pkg.IsWebsocket(c){
+		watchAllPVCns(c)
+	}else{
+		getAllPVCns(c)
+	}
+}
+
+func getPVCns(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	req,err := oapi.GenRequest("GET","/api/v1/namespaces/"+namespace+"/persistentvolumeclaims/"+name,token,nil)
+	if err != nil{
+		logger.Error("Get A PersistentVolumeClaim In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func getAllPVC(c *gin.Context){
+	token := pkg.GetToken(c)
+	req,err := oapi.GenRequest("GET","/api/v1/persistentvolumeclaims",token,nil)
+	if err != nil{
+		logger.Error("Get All PersistentVolumeClaims Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func getAllPVCns(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	req,err := oapi.GenRequest("GET","/api/v1/namespaces/"+namespace+"/persistentvolumeclaims",token,nil)
+	if err != nil{
+		logger.Error("Get All PersistentVolumeClaim In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func watchPVCns(c *gin.Context){
+
+	token := pkg.GetWSToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	oapi.WSRequest("/api/v1/watch/namespaces/"+namespace+"/persistentvolumeclaims/"+name,token,c.Writer,c.Request)
+
+}
+
+func watchAllPVC(c *gin.Context){
+
+	token := pkg.GetWSToken(c)
+	oapi.WSRequest("/api/v1/watch/persistentvolumeclaims",token,c.Writer,c.Request)
+
+}
+
+func watchAllPVCns(c *gin.Context){
+
+	token := pkg.GetWSToken(c)
+	namespace := c.Param("namespace")
+	oapi.WSRequest("/api/v1/watch/namespaces/"+namespace+"/persistentvolumeclaims",token,c.Writer,c.Request)
+
+}
+
+func UpdatePVCns(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	req,err := oapi.GenRequest("PUT","/api/v1/namespaces/"+namespace+"/persistentvolumeclaims/"+name,token,rBody)
+	if err != nil{
+		logger.Error("Update A PersistentVolumeClaim In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func PatchPVCns(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	req,err := oapi.GenRequest("PATCH","/api/v1/namespaces/"+namespace+"/persistentvolumeclaims/"+name,token,rBody)
+	if err != nil{
+		logger.Error("Patch A PersistentVolumeClaim In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func DeletePVCns(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	req,err := oapi.GenRequest("DELETE","/api/v1/namespaces/"+namespace+"/persistentvolumeclaims/"+name,token,rBody)
+	if err != nil{
+		logger.Error("Delete A PersistentVolumeClaim In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func DeleteAllPVCns(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	req,err := oapi.GenRequest("DELETE","/api/v1/namespaces/"+namespace+"/persistentvolumeclaims",token,nil)
+	if err != nil{
+		logger.Error("Delete All PersistentVolumeClaim In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func GetstatusofPVCns(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	req,err := oapi.GenRequest("GET","/api/v1/namespaces/"+namespace+"/persistentvolumeclaims/"+name+"/status",token,nil)
+	if err != nil{
+		logger.Error("Get status of a PersistentVolumeClaim In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func UpdatestatusofPVCns(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	req,err := oapi.GenRequest("PUT","/api/v1/namespaces/"+namespace+"/persistentvolumeclaims/"+name+"/status",token,rBody)
+	if err != nil{
+		logger.Error("Update status of a PersistentVolumeClaim In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+func PatchstatusofPVCns(c *gin.Context){
+	token := pkg.GetToken(c)
+	namespace := c.Param("namespace")
+	name := c.Param("name")
+	rBody,_ := ioutil.ReadAll(c.Request.Body)
+	req,err := oapi.GenRequest("PATCH","/api/v1/namespaces/"+namespace+"/persistentvolumeclaims/"+name+"/status",token,rBody)
+	if err != nil{
+		logger.Error("Patch status of a PersistentVolumeClaim In A Namespace Fail",err)
+	}
+	result, _:= ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	c.Data(req.StatusCode, "application/json",result)
+}
+
+//v1.PersistentVolumeClaim
+//router.POST("/api/v1/persistentvolumeclaims", route.CreatePVC)
+//router.POST("/api/v1/namespaces/:namespace/persistentvolumeclaims", route.CreatePVCns)
+//router.GET("/api/v1/namespaces/:namespace/persistentvolumeclaims/:name", route.GorWPVCns)
+//router.GET("/api/v1/persistentvolumeclaims", route.GorWAllPVC)
+//router.GET("/api/v1/namespaces/:namespace/persistentvolumeclaims", route.GorWAllPVCns)
+//router.PUT("/api/v1/namespaces/:namespace/persistentvolumeclaims/:name", route.UpdatePVCns)
+//router.PATCH("/api/v1/namespaces/:namespace/persistentvolumeclaims/:name", route.PatchPVCns)
+//router.DELETE("/api/v1/namespaces/:namespace/persistentvolumeclaims/:name", route.DeletePVCns)
+//router.DELETE("/api/v1/namespaces/:namespace/persistentvolumeclaims", route.DeleteAllPVCns)
+//router.GET("/api/v1/namespaces/:namespace/persistentvolumeclaims/:name/status", route.GetstatusofPVCns)
+//router.PUT("/api/v1/namespaces/:namespace/persistentvolumeclaims/:name/status", route.UpdatestatusofPVCns)
+//router.PATCH("/api/v1/namespaces/:namespace/persistentvolumeclaims/:name/status", route.PatchstatusofPVCns)
